481f050fd8074602f8928c54d8f7168b76929747
anuchan <anuamd@gmail.com>
2017-06-28 18:48:27 -0700

    Enabling user to poll long running operations

--- azure-client-runtime/src/main/java/com/microsoft/azure/PollingState.java
+++ azure-client-runtime/src/main/java/com/microsoft/azure/PollingState.java
@@ -6,6 +6,7 @@
 
 package com.microsoft.azure;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.microsoft.rest.protocol.SerializerAdapter;
 import okhttp3.ResponseBody;
@@ -15,41 +16,56 @@ import java.io.IOException;
 import java.lang.reflect.Type;
 
 /**
- * An instance of this class defines the state of a long running operation.
+ * An instance of this class defines polling status of a long running operation.
  *
  * @param <T> the type of the resource the operation returns.
  */
-final class PollingState<T> {
-    /** The Retrofit response object. */
-    private Response<ResponseBody> response;
+public class PollingState<T> {
+    /** The HTTP method used to initiate the long running operation **/
+    private String initialHttpMethod;
     /** The polling status. */
     private String status;
     /** The link in 'Azure-AsyncOperation' header. */
     private String azureAsyncOperationHeaderLink;
     /** The link in 'Location' Header. */
     private String locationHeaderLink;
-    /** The timeout interval between two polling operations. */
+    /** The default timeout interval between two polling operations. */
+    private final int defaultRetryTimeout;
+    /** The timeout interval between two polling operation. **/
     private int retryTimeout;
+    /** The resource uri on which PUT or PATCH operation is applied. **/
+    private String putOrPatchResourceUri;
+
+    // Non-serializable properties
+    //
+    /** The Retrofit response object. */
+    @JsonIgnore
+    private Response<ResponseBody> response;
     /** The response resource object. */
+    @JsonIgnore
     private T resource;
     /** The type of the response resource object. */
+    @JsonIgnore
     private Type resourceType;
     /** The error during the polling operations. */
+    @JsonIgnore
     private CloudError error;
     /** The adapter for a custom serializer. */
+    @JsonIgnore
     private SerializerAdapter<?> serializerAdapter;
 
     /**
      * Initializes an instance of {@link PollingState}.
      *
-     * @param response the response from Retrofit REST call.
-     * @param retryTimeout the long running operation retry timeout.
+     * @param response the response from Retrofit REST call that initiate the long running operation.
+     * @param defaultRetryTimeout the long running operation retry timeout.
      * @param resourceType the type of the resource the long running operation returns
      * @param serializerAdapter the adapter for the Jackson object mapper
      * @throws IOException thrown by deserialization
      */
-    PollingState(Response<ResponseBody> response, int retryTimeout, Type resourceType, SerializerAdapter<?> serializerAdapter) throws IOException {
-        this.retryTimeout = retryTimeout;
+    PollingState(Response<ResponseBody> response, int defaultRetryTimeout, Type resourceType, SerializerAdapter<?> serializerAdapter) throws IOException {
+        this.initialHttpMethod = response.raw().request().method();
+        this.defaultRetryTimeout = defaultRetryTimeout;
         this.withResponse(response);
         this.resourceType = resourceType;
         this.serializerAdapter = serializerAdapter;
@@ -81,6 +97,51 @@ final class PollingState<T> {
                     withStatus(AzureAsyncOperation.FAILED_STATUS);
             }
         }
+    }
+
+    /**
+     * Gets the resource.
+     *
+     * @return the resource.
+     */
+    public T resource() {
+        return resource;
+    }
+
+    /**
+     * Gets the operation response.
+     *
+     * @return the operation response.
+     */
+    public Response<ResponseBody> response() {
+        return this.response;
+    }
+
+    /**
+     * Gets the polling status.
+     *
+     * @return the polling status.
+     */
+    public String status() {
+        return status;
+    }
+
+    /**
+     * Gets the value captured from Azure-AsyncOperation header.
+     *
+     * @return the link in the header.
+     */
+    public String azureAsyncOperationHeaderLink() {
+        return azureAsyncOperationHeaderLink;
+    }
+
+    /**
+     * Gets the value captured from Location header.
+     *
+     * @return the link in the header.
+     */
+    public String locationHeaderLink() {
+        return locationHeaderLink;
     }
 
     /**
@@ -141,28 +202,50 @@ final class PollingState<T> {
      */
     int delayInMilliseconds() {
         if (this.retryTimeout >= 0) {
-            return this.retryTimeout * 1000;
+            return this.retryTimeout;
         }
-        if (this.response != null && response.headers().get("Retry-After") != null) {
-            return Integer.parseInt(response.headers().get("Retry-After")) * 1000;
+        if (this.defaultRetryTimeout >= 0) {
+            return this.defaultRetryTimeout * 1000;
         }
         return AzureAsyncOperation.DEFAULT_DELAY * 1000;
     }
 
     /**
-     * Gets the polling status.
-     *
-     * @return the polling status.
+     * @return the uri of the resource on which the LRO PUT or PATCH applied.
      */
-    String status() {
-        return status;
+    String putOrPatchResourceUri() {
+        return this.putOrPatchResourceUri;
     }
 
     /**
-     * @return the resource type
+     * @return true if the status this state hold represents terminal status.
      */
-    Type resourceType() {
-        return resourceType;
+    boolean isStatusTerminal() {
+        for (String terminalStatus : AzureAsyncOperation.terminalStatuses()) {
+            if (terminalStatus.equalsIgnoreCase(this.status())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * @return true if the status this state hold is represents failed status.
+     */
+    boolean isStatusFailed() {
+        for (String failedStatus : AzureAsyncOperation.failedStatuses()) {
+            if (failedStatus.equalsIgnoreCase(this.status())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * @return true if the status this state represents is succeeded status.
+     */
+    boolean isStatusSucceeded() {
+        return AzureAsyncOperation.SUCCESS_STATUS.equalsIgnoreCase(this.status());
     }
 
     /**
@@ -180,22 +263,18 @@ final class PollingState<T> {
     }
 
     /**
-     * Gets the last operation response.
-     *
-     * @return the last operation response.
-     */
-    Response<ResponseBody> response() {
-        return this.response;
-    }
-
-
-    /**
      * Sets the last operation response.
      *
      * @param response the last operation response.
      */
     PollingState<T> withResponse(Response<ResponseBody> response) {
         this.response = response;
+        withPollingUrlFromResponse(response);
+        withPollingRetryTimeoutFromResponse(response);
+        return this;
+    }
+
+    PollingState<T> withPollingUrlFromResponse(Response<ResponseBody> response) {
         if (response != null) {
             String asyncHeader = response.headers().get("Azure-AsyncOperation");
             String locationHeader = response.headers().get("Location");
@@ -209,31 +288,18 @@ final class PollingState<T> {
         return this;
     }
 
-    /**
-     * Gets the latest value captured from Azure-AsyncOperation header.
-     *
-     * @return the link in the header.
-     */
-    String azureAsyncOperationHeaderLink() {
-        return azureAsyncOperationHeaderLink;
+    PollingState<T> withPollingRetryTimeoutFromResponse(Response<ResponseBody> response) {
+        if (this.response != null && response.headers().get("Retry-After") != null) {
+            retryTimeout = Integer.parseInt(response.headers().get("Retry-After")) * 1000;
+            return this;
+        }
+        this.retryTimeout = -1;
+        return this;
     }
 
-    /**
-     * Gets the latest value captured from Location header.
-     *
-     * @return the link in the header.
-     */
-    String locationHeaderLink() {
-        return locationHeaderLink;
-    }
-
-    /**
-     * Gets the resource.
-     *
-     * @return the resource.
-     */
-    T resource() {
-        return resource;
+    PollingState<T> withPutOrPatchResourceUri(final String uri) {
+        this.putOrPatchResourceUri = uri;
+        return this;
     }
 
     /**
@@ -243,6 +309,23 @@ final class PollingState<T> {
      */
     PollingState<T> withResource(T resource) {
         this.resource = resource;
+        return this;
+    }
+
+    /**
+     * @return the resource type
+     */
+    Type resourceType() {
+        return resourceType;
+    }
+
+    /**
+     * Sets resource type.
+     *
+     * param resourceType the resource type
+     */
+    PollingState<T> withResourceType(Type resourceType) {
+        this.resourceType = resourceType;
         return this;
     }
 
@@ -263,6 +346,36 @@ final class PollingState<T> {
     PollingState<T> withErrorBody(CloudError error) {
         this.error = error;
         return this;
+    }
+
+    /**
+     * Sets the serializer adapter.
+     *
+     * @param serializerAdapter the serializer adapter.
+     */
+    PollingState<T> withSerializerAdapter(SerializerAdapter<?> serializerAdapter) {
+        this.serializerAdapter = serializerAdapter;
+        return this;
+    }
+
+    /**
+     * @return the http method used to initiate the long running operation.
+     */
+    String initialHttpMethod() {
+        return this.initialHttpMethod;
+    }
+
+    /**
+     * If status is in failed state then throw CloudException.
+     */
+    void throwCloudExceptionIfInFailedState() {
+        if (this.isStatusFailed()) {
+            if (this.errorBody() != null) {
+                throw new CloudException("Async operation failed with provisioning state: " + this.status(), this.response(), this.errorBody());
+            } else {
+                throw new CloudException("Async operation failed with provisioning state: " + this.status(), this.response());
+            }
+        }
     }
 
     /**

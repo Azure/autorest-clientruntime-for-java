f9111264e5d38a8003f22a72d05335e320f7e67f
Jianghao Lu <jianghaolu@users.noreply.github.com>
2017-08-07 16:37:23 -0700

    Add headers and contexts

---
+++ client-runtime/src/main/java/com/microsoft/rest/v2/annotations/Contexts.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ */
+
+package com.microsoft.rest.v2.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Retention(RUNTIME)
+@Target(METHOD)
+public @interface Contexts {
+    /**
+     * List of static contexts.
+     */
+    String[] value();
+}

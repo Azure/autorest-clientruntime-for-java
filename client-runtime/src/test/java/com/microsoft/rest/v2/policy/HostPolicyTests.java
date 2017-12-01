704706272413d31c73302e375d8dd46390f78285
Dan Schulte <daschult@microsoft.com>
2017-12-01 14:48:12 -0800

    Add HostPolicy tests with port

--- client-runtime/src/test/java/com/microsoft/rest/v2/policy/HostPolicyTests.java
+++ client-runtime/src/test/java/com/microsoft/rest/v2/policy/HostPolicyTests.java
@@ -9,15 +9,27 @@ import static org.junit.Assert.assertEquals;
 
 public class HostPolicyTests {
     @Test
-    public void withNoProtocol() {
+    public void withNoProtocolAndNoPort() {
         final HostPolicy policy = createHostPolicy("localhost", "localhost");
         policy.sendAsync(createHttpRequest("www.bing.com"));
     }
 
     @Test
-    public void withProtocol() {
+    public void withProtocolAndNoPort() {
         final HostPolicy policy = createHostPolicy("localhost", "ftp://localhost");
         policy.sendAsync(createHttpRequest("ftp://www.example.com"));
+    }
+
+    @Test
+    public void withNoProtocolAndPort() {
+        final HostPolicy policy = createHostPolicy("localhost", "localhost:8080");
+        policy.sendAsync(createHttpRequest("www.bing.com:8080"));
+    }
+
+    @Test
+    public void withProtocolAndPort() {
+        final HostPolicy policy = createHostPolicy("localhost", "ftp://localhost:1234");
+        policy.sendAsync(createHttpRequest("ftp://www.example.com:1234"));
     }
 
     private static RequestPolicy createMockRequestPolicy(final String expectedUrl) {

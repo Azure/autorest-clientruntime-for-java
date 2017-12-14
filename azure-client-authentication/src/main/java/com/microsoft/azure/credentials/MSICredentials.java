9113032730f700f5aae68cd9d5962ba40a3dc65a
Anu Thomas Chandy <anuamd@hotmail.com>
2017-12-14 10:24:06 -0800

    Updating MSICredentails to support login based on user assigned identity (#324)

--- azure-client-authentication/src/main/java/com/microsoft/azure/credentials/MSICredentials.java
+++ azure-client-authentication/src/main/java/com/microsoft/azure/credentials/MSICredentials.java
@@ -27,11 +27,24 @@ public class MSICredentials extends AzureTokenCredentials {
     private final String resource;
     private final int msiPort;
     private final AzureJacksonAdapter adapter;
+    private String objectId;
+    private String clientId;
+    private String identityId;
+
+
+    /**
+     * Initializes a new instance of the MSICredentials.
+     */
+    public MSICredentials() {
+        this(AzureEnvironment.AZURE);
+    }
+
     /**
      * Initializes a new instance of the MSICredentials.
      *
      * @param environment the Azure environment to use
      */
+
     public MSICredentials(AzureEnvironment environment) {
         this(environment, 50342);
     }
@@ -49,10 +62,62 @@ public class MSICredentials extends AzureTokenCredentials {
         this.adapter = new AzureJacksonAdapter();
     }
 
+    /**
+     * Specifies the object id associated with a user assigned managed service identity
+     * resource that should be used to retrieve the access token.
+     *
+     * @param objectId Object ID of the identity to use when authenticating to Azure AD.
+     * @return MSICredentials
+     */
+    @Beta
+    public MSICredentials withObjectId(String objectId) {
+        this.objectId = objectId;
+        this.clientId = null;
+        this.identityId = null;
+        return this;
+    }
+
+    /**
+     * Specifies the application id (client id) associated with a user assigned managed service identity
+     * resource that should be used to retrieve the access token.
+     *
+     * @param clientId application id (client id) of the identity to use when authenticating to Azure AD.
+     * @return MSICredentials
+     */
+    @Beta
+    public MSICredentials withClientId(String clientId) {
+        this.clientId = clientId;
+        this.objectId = null;
+        this.identityId = null;
+        return this;
+    }
+
+    /**
+     * Specifies the ARM resource id of the user assigned managed service identity resource that
+     * should be used to retrieve the access token.
+     *
+     * @param identityId the ARM resource id of the user assigned identity resource
+     * @return MSICredentials
+     */
+    @Beta
+    public MSICredentials withIdentityId(String identityId) {
+        this.identityId = identityId;
+        this.clientId = null;
+        this.objectId = null;
+        return this;
+    }
+
     @Override
     public String getToken(String resource) throws IOException {
         URL url = new URL(String.format("http://localhost:%d/oauth2/token", this.msiPort));
         String postData = String.format("resource=%s", this.resource);
+        if (this.objectId != null) {
+            postData += String.format("&object_id=%s", this.objectId);
+        } else if (this.clientId != null) {
+            postData += String.format("&client_id=%s", this.clientId);
+        } else if (this.identityId != null) {
+            postData += String.format("&msi_res_id=%s", this.identityId);
+        }
         HttpURLConnection connection = null;
 
         try {
@@ -76,6 +141,9 @@ public class MSICredentials extends AzureTokenCredentials {
 
             MSIToken msiToken = adapter.deserialize(result, MSIToken.class);
             return msiToken.accessToken;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
         } finally {
             if (connection != null) {
                 connection.disconnect();

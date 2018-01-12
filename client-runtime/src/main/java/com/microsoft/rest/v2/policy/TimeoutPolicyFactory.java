e64235b3af49275c409454a1450415ce4925da27
Rikki Gibson <rigibson@microsoft.com>
2018-01-11 16:56:06 -0800

    Add TimeoutPolicyFactory. Delete UseOtherHostPolicy.

---
+++ client-runtime/src/main/java/com/microsoft/rest/v2/policy/TimeoutPolicyFactory.java
@@ -0,0 +1,42 @@
+package com.microsoft.rest.v2.policy;
+
+import com.microsoft.rest.v2.http.HttpRequest;
+import com.microsoft.rest.v2.http.HttpResponse;
+import io.reactivex.Single;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * RequestPolicy which limits the time allowed between sending a request and receiving the response.
+ */
+public final class TimeoutPolicyFactory implements RequestPolicyFactory {
+    private final long timeout;
+    private final TimeUnit unit;
+
+    /**
+     * Creates a TimeoutPolicyFactory
+     * @param timeout the length of the timeout
+     * @param unit the unit of the timeout
+     */
+    public TimeoutPolicyFactory(long timeout, TimeUnit unit) {
+        this.timeout = timeout;
+        this.unit = unit;
+    }
+
+    @Override
+    public RequestPolicy create(RequestPolicy next, RequestPolicyOptions options) {
+        return new TimeoutPolicy(next);
+    }
+
+    private final class TimeoutPolicy implements RequestPolicy {
+        private final RequestPolicy next;
+        TimeoutPolicy(RequestPolicy next) {
+            this.next = next;
+        }
+
+        @Override
+        public Single<HttpResponse> sendAsync(HttpRequest request) {
+            return next.sendAsync(request).timeout(timeout, unit);
+        }
+    }
+}

8e119ef3af1baa930bd5cf0ff395d467714369d9
Rikki Gibson <rigibson@microsoft.com>
2018-02-02 12:23:33 -0800

    Add null check to DecodingPolicy

--- client-runtime/src/main/java/com/microsoft/rest/v2/policy/DecodingPolicyFactory.java
+++ client-runtime/src/main/java/com/microsoft/rest/v2/policy/DecodingPolicyFactory.java
@@ -8,6 +8,7 @@ package com.microsoft.rest.v2.policy;
 
 import com.microsoft.rest.v2.http.HttpRequest;
 import com.microsoft.rest.v2.http.HttpResponse;
+import com.microsoft.rest.v2.protocol.HttpResponseDecoder;
 import io.reactivex.Single;
 import io.reactivex.SingleSource;
 import io.reactivex.functions.Function;
@@ -32,7 +33,12 @@ public final class DecodingPolicyFactory implements RequestPolicyFactory {
             return next.sendAsync(request).flatMap(new Function<HttpResponse, SingleSource<? extends HttpResponse>>() {
                 @Override
                 public SingleSource<? extends HttpResponse> apply(final HttpResponse response) throws Exception {
-                    return request.responseDecoder().decode(response);
+                    HttpResponseDecoder decoder = request.responseDecoder();
+                    if (decoder != null) {
+                        return request.responseDecoder().decode(response);
+                    } else {
+                        return Single.error(new NullPointerException("HttpRequest.responseDecoder() was null when decoding."));
+                    }
                 }
             });
         }

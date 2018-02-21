84823f2e7e0c54a1ef19373a137c2943fd618606
Rikki Gibson <rigibson@microsoft.com>
2018-02-20 17:32:44 -0800

    Use daemon mode for SharedChannelPool worker thread

--- client-runtime/src/main/java/com/microsoft/rest/v2/http/SharedChannelPool.java
+++ client-runtime/src/main/java/com/microsoft/rest/v2/http/SharedChannelPool.java
@@ -27,6 +27,7 @@ import java.util.concurrent.ConcurrentLinkedDeque;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.ThreadFactory;
 
 /**
  * A Netty channel pool implementation shared between multiple requests.
@@ -79,7 +80,15 @@ public class SharedChannelPool implements ChannelPool {
         } catch (SSLException e) {
             throw new RuntimeException(e);
         }
-        this.executor = Executors.newSingleThreadExecutor();
+        this.executor = Executors.newSingleThreadExecutor(new ThreadFactory() {
+            @Override
+            public Thread newThread(Runnable r) {
+                Thread thread = new Thread(r, "SharedChannelPool-worker");
+                thread.setDaemon(true);
+                return thread;
+            }
+        });
+
         executor.submit(new Runnable() {
             @Override
             public void run() {

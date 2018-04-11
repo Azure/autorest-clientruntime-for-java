f7df81593f0f43ae3651a339a297a9e3d2e24bac
Anu Thomas Chandy <anuamd@hotmail.com>
2018-04-11 15:00:33 -0700

    Adding support for IMDS based MSI token acquisition & using IMDS as default token retrival endpoint (#419)

--- azure-client-authentication/src/main/java/com/microsoft/azure/credentials/MSICredentials.java
+++ azure-client-authentication/src/main/java/com/microsoft/azure/credentials/MSICredentials.java
@@ -17,15 +17,30 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Managed Service Identity token based credentials for use with a REST Service Client.
  */
 @Beta
 public class MSICredentials extends AzureTokenCredentials {
+    //
+    private final List<Integer> retrySlots = new ArrayList<>(Arrays.asList(new Integer[] {1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765}));
+    private final int maxRetry = retrySlots.size();
+    private final Lock lock = new ReentrantLock();
+    private final ConcurrentHashMap<String, MSIToken> cache = new ConcurrentHashMap<>();
+    //
     private final String resource;
-    private final int msiPort;
-    private final AzureJacksonAdapter adapter;
+    private int msiPort = 50342;
+    private final MSITokenSource tokenSource;
+    private final AzureJacksonAdapter adapter = new AzureJacksonAdapter();
     private String objectId;
     private String clientId;
     private String identityId;
@@ -43,9 +58,10 @@ public class MSICredentials extends AzureTokenCredentials {
      *
      * @param environment the Azure environment to use
      */
-
     public MSICredentials(AzureEnvironment environment) {
-        this(environment, 50342);
+        super(environment, null /** retrieving MSI token does not require tenant **/);
+        this.resource = environment.resourceManagerEndpoint();
+        this.tokenSource = MSITokenSource.IMDS_ENDPOINT;
     }
 
     /**
@@ -53,12 +69,14 @@ public class MSICredentials extends AzureTokenCredentials {
      *
      * @param environment the Azure environment to use
      * @param msiPort the local port to retrieve token from
+     * @deprecated use {@link #MSICredentials()} or {@link #MSICredentials(AzureEnvironment)} instead.
      */
+    @Deprecated()
     public MSICredentials(AzureEnvironment environment, int msiPort) {
         super(environment, null /** retrieving MSI token does not require tenant **/);
         this.resource = environment.resourceManagerEndpoint();
         this.msiPort = msiPort;
-        this.adapter = new AzureJacksonAdapter();
+        this.tokenSource = MSITokenSource.MSI_EXTENSION;
     }
 
     /**
@@ -106,8 +124,17 @@ public class MSICredentials extends AzureTokenCredentials {
         return this;
     }
 
+
     @Override
     public String getToken(String resource) throws IOException {
+        if (this.tokenSource == MSITokenSource.MSI_EXTENSION) {
+            return this.getTokenFromMSIExtension();
+        } else {
+            return this.getTokenFromIMDSEndpoint();
+        }
+    }
+
+    private String getTokenFromMSIExtension() throws IOException {
         URL url = new URL(String.format("http://localhost:%d/oauth2/token", this.msiPort));
         String postData = String.format("resource=%s", this.resource);
         if (this.objectId != null) {
@@ -116,6 +143,8 @@ public class MSICredentials extends AzureTokenCredentials {
             postData += String.format("&client_id=%s", this.clientId);
         } else if (this.identityId != null) {
             postData += String.format("&msi_res_id=%s", this.identityId);
+        } else {
+            throw new IllegalArgumentException("objectId, clientId or identityId must be set");
         }
         HttpURLConnection connection = null;
 
@@ -148,5 +177,120 @@ public class MSICredentials extends AzureTokenCredentials {
                 connection.disconnect();
             }
         }
+    }
+
+    private String getTokenFromIMDSEndpoint() {
+        MSIToken token = cache.get(resource);
+        if (token != null && !token.isExpired()) {
+            return token.accessToken();
+        }
+        lock.lock();
+        try {
+            token = cache.get(resource);
+            if (token != null && !token.isExpired()) {
+                return token.accessToken();
+            }
+            try {
+                token = retrieveTokenFromIDMSWithRetry();
+                if (token != null) {
+                    cache.put(resource, token);
+                }
+            } catch (IOException exception) {
+                throw new RuntimeException(exception);
+            }
+            return token.accessToken();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    private MSIToken retrieveTokenFromIDMSWithRetry() throws IOException {
+        StringBuilder payload = new StringBuilder();
+        //
+        try {
+            payload.append("api-version");
+            payload.append("=");
+            payload.append(URLEncoder.encode("2018-02-01", "UTF-8"));
+            payload.append("&");
+            payload.append("resource");
+            payload.append("=");
+            payload.append(URLEncoder.encode(this.resource, "UTF-8"));
+            payload.append("&");
+            if (this.objectId != null) {
+                payload.append("object_id");
+                payload.append("=");
+                payload.append(URLEncoder.encode(this.objectId, "UTF-8"));
+            } else if (this.clientId != null) {
+                payload.append("client_id");
+                payload.append("=");
+                payload.append(URLEncoder.encode(this.clientId, "UTF-8"));
+            } else if (this.identityId != null) {
+                payload.append("msi_res_id");
+                payload.append("=");
+                payload.append(URLEncoder.encode(this.identityId, "UTF-8"));
+            } else {
+                throw new IllegalArgumentException("objectId, clientId or identityId must be set");
+            }
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+
+        int retry = 1;
+        while (retry <= maxRetry) {
+            URL url = new URL(String.format("http://169.254.169.254/metadata/identity/oauth2/token?%s", payload.toString()));
+            //
+            HttpURLConnection connection = null;
+            //
+            try {
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.setRequestProperty("Metadata", "true");
+                connection.connect();
+                InputStream stream = connection.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(stream, "UTF-8"), 100);
+                String result = reader.readLine();
+                return adapter.deserialize(result, MSIToken.class);
+            } catch (Exception exception) {
+                int responseCode = connection.getResponseCode();
+                if (responseCode == 429 || responseCode == 404 || (responseCode >= 500 && responseCode <= 599)) {
+                    int retryTimeout = retrySlots.get(new Random().nextInt(retry)) * 1000;
+                    sleep(retryTimeout);
+                    retry++;
+                } else {
+                    throw new RuntimeException(exception);
+                }
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+            }
+        }
+        //
+        if (retry > maxRetry) {
+            throw new RuntimeException(String.format("MSI: Failed to acquire tokens after retrying %s times", maxRetry));
+        }
+        return null;
+    }
+
+    private static void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    /**
+     * The source of MSI token.
+     */
+    private enum MSITokenSource {
+        /**
+         * Indicate that token should be retrieved from MSI extension installed in the VM.
+         */
+        MSI_EXTENSION,
+        /**
+         * Indicate that token should be retrieved from IMDS service.
+         */
+        IMDS_ENDPOINT
     }
 }

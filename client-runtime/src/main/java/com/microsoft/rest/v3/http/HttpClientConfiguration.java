12f6781da4a88c50ed31af8af5a3e566306be186
Jianghao Lu <jianghaolu@users.noreply.github.com>
2019-01-30 16:04:56 -0800

    Support proxy

--- client-runtime/src/main/java/com/microsoft/rest/v3/http/HttpClientConfiguration.java
+++ client-runtime/src/main/java/com/microsoft/rest/v3/http/HttpClientConfiguration.java
@@ -6,42 +6,29 @@
 
 package com.microsoft.rest.v3.http;
 
-import com.microsoft.rest.v3.annotations.Beta;
-
-import java.net.Proxy;
-
 /**
  * The set of parameters used to create an HTTP client.
  */
 public class HttpClientConfiguration {
-    private final Proxy proxy;
-    private SharedChannelPoolOptions poolOptions;
+    private ProxyOptions proxy;
+    private final SharedChannelPoolOptions poolOptions;
 
     /**
      * @return The optional proxy to use.
      */
-    public Proxy proxy() {
+    public ProxyOptions proxy() {
         return proxy;
     }
 
     /**
      * Creates an HttpClientConfiguration.
-     * @param proxy The optional proxy to use.
      */
-    public HttpClientConfiguration(Proxy proxy) {
-        this.proxy = proxy;
+    public HttpClientConfiguration() {
         this.poolOptions = new SharedChannelPoolOptions();
     }
 
-    /**
-     * Sets the duration in sec to keep the connection alive in available pool before closing it.
-     *
-     * @param duration duration in seconds
-     * @return HttpClientConfiguration
-     */
-    @Beta(since = "2.0.0")
-    public HttpClientConfiguration withIdleConnectionKeepAliveDurationInSec(long duration) {
-        this.poolOptions.withIdleChannelKeepAliveDurationInSec(duration);
+    public HttpClientConfiguration withProxy(ProxyOptions proxyOptions) {
+        this.proxy = proxyOptions;
         return this;
     }
 }

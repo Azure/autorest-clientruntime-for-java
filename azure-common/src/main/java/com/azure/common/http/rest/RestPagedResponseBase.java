4d5e7b9811fa41b2f5aaa1e0d9637c4c847b37df
Connie <conniey@microsoft.com>
2019-03-18 15:43:46 -0700

    Adding RestPagedResponseBase class

---
+++ azure-common/src/main/java/com/azure/common/http/rest/RestPagedResponseBase.java
@@ -0,0 +1,77 @@
+package com.azure.common.http.rest;
+
+import com.azure.common.http.HttpHeaders;
+import com.azure.common.http.HttpRequest;
+
+import java.util.List;
+
+public class RestPagedResponseBase<H, T extends Page<T>> implements RestPagedResponse<T> {
+    private final HttpRequest request;
+    private final int statusCode;
+    private final H deserializedHeaders;
+    private final HttpHeaders headers;
+    private final List<T> items;
+    private final String nextLink;
+
+    public RestPagedResponseBase(HttpRequest request, int statusCode, HttpHeaders headers, T body, H deserializedHeaders) {
+        this.request = request;
+        this.statusCode = statusCode;
+        this.headers = headers;
+        this.items = body.items();
+        this.nextLink = body.nextLink();
+        this.deserializedHeaders = deserializedHeaders;
+    }
+
+    public RestPagedResponseBase(HttpRequest request, int statusCode, HttpHeaders headers, List<T> items, String nextLink, H deserializedHeaders) {
+        this.request = request;
+        this.statusCode = statusCode;
+        this.headers = headers;
+        this.items = items;
+        this.nextLink = nextLink;
+        this.deserializedHeaders = deserializedHeaders;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<T> items() { return items; }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String nextLink() { return nextLink; }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int statusCode() { return statusCode; }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public HttpHeaders headers() { return headers; }
+
+    /**
+     * @return the request which resulted in this PagedRequestResponse.
+     */
+    @Override
+    public HttpRequest request() { return request; }
+
+    /**
+     * Get the headers from the HTTP response, transformed into the header type H.
+     *
+     * @return an instance of header type H, containing the HTTP response headers.
+     */
+    public H deserializedHeaders() {
+        return deserializedHeaders;
+    }
+
+    @Override
+    public void close() {
+
+    }
+}

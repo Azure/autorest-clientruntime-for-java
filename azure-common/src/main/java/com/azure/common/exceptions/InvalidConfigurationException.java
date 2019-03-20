b0960e7dd476451143f44a869f80957aa5279da0
Connie <conniey@microsoft.com>
2019-03-20 15:29:49 -0700

    Adding Exception during validation

---
+++ azure-common/src/main/java/com/azure/common/exceptions/InvalidConfigurationException.java
@@ -0,0 +1,7 @@
+package com.azure.common.exceptions;
+
+public class InvalidConfigurationException extends RuntimeException {
+    public InvalidConfigurationException(String message) {
+        super(message);
+    }
+}

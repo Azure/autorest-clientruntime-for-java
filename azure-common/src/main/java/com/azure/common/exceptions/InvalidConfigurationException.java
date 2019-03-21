c0f599ffaa090fbe708d390bb6bb862c125dbc33
Connie <conniey@microsoft.com>
2019-03-21 12:22:50 -0700

    Removing unused exception

--- azure-common/src/main/java/com/azure/common/exceptions/InvalidConfigurationException.java
+++
@@ -1,7 +0,0 @@
-package com.azure.common.exceptions;
-
-public class InvalidConfigurationException extends RuntimeException {
-    public InvalidConfigurationException(String message) {
-        super(message);
-    }
-}

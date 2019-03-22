90a62275fb2fc5eeea918931e42a48780925d0e5
Connie <conniey@microsoft.com>
2019-03-21 21:47:44 -0700

    Add documentation for AsyncServiceClientCredentials

--- azure-common/src/main/java/com/azure/common/credentials/AsyncServiceClientCredentials.java
+++ azure-common/src/main/java/com/azure/common/credentials/AsyncServiceClientCredentials.java
@@ -10,10 +10,12 @@ import com.azure.common.http.HttpRequest;
 import reactor.core.publisher.Mono;
 
 /**
- * Provides credentials to be put in the HTTP Authorization header.
+ * Provides credentials for an HTTP request's 'Authorization' header.
  */
 public interface AsyncServiceClientCredentials {
     /**
+     * Given the {@param httpRequest}, generates a value for the 'Authorization' header.
+     *
      * @param httpRequest The HTTP request that requires an authorization header.
      * @return The value containing currently valid credentials to put in the HTTP header, 'Authorization'.
      */

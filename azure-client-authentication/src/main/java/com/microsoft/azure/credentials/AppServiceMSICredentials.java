c32b146a0a99a37ec740cb74044fe46cd448fda2
Pareekshit Singh <35941423+praries880@users.noreply.github.com>
2019-06-21 14:27:05 -0700

    Add support for User assigned identitied to the app service cred (#622)
    * Add support for User assigned identitied to the app service cred
    provider
    Fix for issue #617
    
    * remove object ID

--- azure-client-authentication/src/main/java/com/microsoft/azure/credentials/AppServiceMSICredentials.java
+++ azure-client-authentication/src/main/java/com/microsoft/azure/credentials/AppServiceMSICredentials.java
@@ -25,6 +25,7 @@ public class AppServiceMSICredentials extends AzureTokenCredentials {
     private final String endpoint;
     private final String secret;
     private final AzureJacksonAdapter adapter;
+    private String clientId;
 
     /**
      * Creates an MSI credential for app services.
@@ -53,9 +54,30 @@ public class AppServiceMSICredentials extends AzureTokenCredentials {
         this.adapter = new AzureJacksonAdapter();
     }
 
+    /**
+     * Specifies the application id (client id) associated with a user assigned managed service identity
+     * resource that should be used to retrieve the access token.
+     *
+     * @param clientId application id (client id) of the identity to use when authenticating to Azure AD.
+     * @return AppServiceMSICredentials
+     */
+    public AppServiceMSICredentials withClientId(String clientId) {
+        this.clientId = clientId;
+        return this;
+    }
+
     @Override
     public String getToken(String resource) throws IOException {
-        String urlString = String.format("%s?resource=%s&api-version=2017-09-01", this.endpoint, resource);
+        String urlString = null;
+
+        if (this.clientId != null && !this.clientId.isEmpty()) {
+            urlString = String.format("%s?resource=%s&clientid=%s&api-version=2017-09-01", this.endpoint,
+                    resource, this.clientId);
+        } else {
+            urlString = String.format("%s?resource=%s&api-version=2017-09-01", this.endpoint,
+                    resource);
+        }
+
         URL url = new URL(urlString);
         HttpURLConnection connection = null;
 

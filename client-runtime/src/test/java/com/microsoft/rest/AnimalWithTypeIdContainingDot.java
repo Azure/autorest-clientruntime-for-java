e311ab45cc6c87eefda937c172a242259e666c76
Anu Thomas Chandy <anuamd@hotmail.com>
2019-07-20 11:02:26 -0700

    Adding defaultImpl annotation to test types and enable tests for defaultImpl

--- client-runtime/src/test/java/com/microsoft/rest/AnimalWithTypeIdContainingDot.java
+++ client-runtime/src/test/java/com/microsoft/rest/AnimalWithTypeIdContainingDot.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 
-@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "@odata\\.type")
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "@odata\\.type", defaultImpl = AnimalWithTypeIdContainingDot.class)
 @JsonTypeName("AnimalWithTypeIdContainingDot")
 @JsonSubTypes({
         @JsonSubTypes.Type(name = "#Favourite.Pet.DogWithTypeIdContainingDot", value = DogWithTypeIdContainingDot.class),
--- client-runtime/src/test/java/com/microsoft/rest/DogWithTypeIdContainingDot.java
+++ client-runtime/src/test/java/com/microsoft/rest/DogWithTypeIdContainingDot.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 
-@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "@odata\\.type")
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "@odata\\.type", defaultImpl = DogWithTypeIdContainingDot.class)
 @JsonTypeName("#Favourite.Pet.DogWithTypeIdContainingDot")
 public class DogWithTypeIdContainingDot extends AnimalWithTypeIdContainingDot {
     @JsonProperty(value = "breed")
